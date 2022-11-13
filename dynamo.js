a8ead3b59cedf75cf17ac21eb23143bc28bc94d8
anzhelaAbitova <55100446+anzhelaAbitova@users.noreply.github.com>
2022-11-13 13:21:48 +0400

    no config

--- dynamo.js
+++ dynamo.js
@@ -1,4 +1,4 @@
-let AWS = require('aws-sdk');
+
 let express = require('express');
 let router = express.Router();
 
@@ -7,28 +7,13 @@ let table = "sports";
 
 router.get('/fetch', (req, res) => {
 
-let spid = '101';
-let params = {
-    TableName: table,
-    Key: {
-        spid: spid
-    }
-};
+    let spid = '101';
+    let params = {
+        TableName: table,
+        Key: {
+            spid: spid
+        }
+    };
 
-docClient.get(params, function (err, data) {
-    if (err) {
-        console.log(err);
-        handleError(err, res);
-    } else {
-        handleSuccess(data.Item, res);
-    }
- });
+
 });
-function handleError(err, res) {
-    res.json({ 'message': 'server side error', statusCode: 500, error: 
-    err });
-}
-
-function handleSuccess(data, res) {
-    res.json({ message: 'success', statusCode: 200, data: data })
-}
--- index.js
+++ index.js
@@ -4,100 +4,116 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express')
 const app = express()
-// const bodyParser = require('body-parser');
-// const methodOverride = require('method-override');
-// const flash = require('express-flash')
-// const cors = require('cors');
+let AWS = require('aws-sdk');
+let docClient = new AWS.DynamoDB.DocumentClient();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const flash = require('express-flash')
+const cors = require('cors');
 // const Receipts = require('./models/receipts').Receipts;
 let dynamoRoute = require('./dynamo');
 const host = '127.0.0.1'
 const port = process.env.PORT || 3000;
 
-// app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(flash())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(flash())
 // app.use(cors())
-// app.use(methodOverride('_method'))
-// app.use(cors())
+app.use(methodOverride('_method'))
+app.use(cors())
 
-// app.use(async (req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', 'https://anastashasuvorova.ru/')
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
-//   res.setHeader('Access-Control-Allow-Credentials', true)
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-//   )
-//   next()
-// })
+app.use(async (req, res, next) => {
+  if (req.xhr) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://anastashasuvorova.ru/')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    )
+    next()
+  } else {
+    res.status(400).end('400 Bad Request')
+  }
+})
 
-// app.post('/receipts', async (req, res) => {
-//     try {
-//         const loginQ = req.body.login || req.query.login
-//         const receiptsQ = req.body.receipts || req.query.receipts
-//         console.log(req.query)
-//         let search = ''
-//         Receipts.findOne({userLogin: loginQ}, (err, obj) => {
-//             if(err){
-//                 return console.log(err);
-//             } 
-//             if (!obj) {
-//                 const receipt = new Receipts ({
-//                     userLogin: loginQ,
-//                     receipts: [receiptsQ]
-//                   })
-//                  receipt.save(function(err){
-//                     if(err) return console.log(err);
-//                     console.log("Сохранен объект", receipt);
-//                 });
-//             } else {
-//                 const index = obj.receipts.indexOf(receiptsQ)
+app.post('/receipts', async (req, res) => {
+  try {
+    const loginQ = req.body.login || req.query.login
+    const receiptsQ = req.body.receipts || req.query.receipts
+    console.log(req.query)
+    let search = ''
+    // Receipts.findOne({userLogin: loginQ}, (err, obj) => {
+    //     if(err){
+    //         return console.log(err);
+    //     } 
+    //     if (!obj) {
+    //         const receipt = new Receipts ({
+    //             userLogin: loginQ,
+    //             receipts: [receiptsQ]
+    //           })
+    //          receipt.save(function(err){
+    //             if(err) return console.log(err);
+    //             console.log("Сохранен объект", receipt);
+    //         });
+    //     } else {
+    //         const index = obj.receipts.indexOf(receiptsQ)
 
-//                 if (index === -1) {
-//                   console.log('index ' + index)
-//                   obj.receipts.push(receiptsQ)
-//                 } else {
-//                   obj.receipts.splice(index, 1);
-//                 }
-//                 console.log(obj.receipts)
-//                 return obj.save()
-//             }
-//         })
+    //         if (index === -1) {
+    //           console.log('index ' + index)
+    //           obj.receipts.push(receiptsQ)
+    //         } else {
+    //           obj.receipts.splice(index, 1);
+    //         }
+    //         console.log(obj.receipts)
+    //         return obj.save()
+    //     }
+    // })
 
-//     } catch (err) {
-//       console.log(err);
-//       return res.sendStatus(500);  
-//     }})
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(500);
+  }
+})
 
-//   app.get('/receipts', (req, res) => {
-//     const loginQ = req.body.login || req.query.login
-//     Receipts.findOne({userLogin: loginQ}, (err, docs) => {
-//       if (err) {
-//         console.log(err);
-//         return res.sendStatus(500);
-//       }
-//       console.log(docs.receipts)
-//       return res.send(docs.receipts);
-//     })
-//   })
+app.get('/receipts', (req, res) => {
+  const loginQ = req.body.login || req.query.login
+  Receipts.findOne({ userLogin: loginQ }, (err, docs) => {
+    if (err) {
+      console.log(err);
+      return res.sendStatus(500);
+    }
+    console.log(docs.receipts)
+    return res.send(docs.receipts);
+  })
+})
 
-//   app.get('/receipt-for-one', (req, res) => {
-//     const loginQ = req.body.login || req.query.login
-//     const receiptsQ = req.body.receipts || req.query.receipts
-//     Receipts.findOne({userLogin: loginQ}, (err, obj) => {
-//       if (err) {
-//         console.log(err);
-//         return res.sendStatus(500);
-//       }
-//       let result = false
-//       const index = obj.receipts.indexOf(receiptsQ)
-//       if (index !== -1) {
-//         result = true
-//       } 
-//       console.log('result' + result)
-//       return res.send(result);
-//     })
-//   })
+app.get('/receipt-for-one', (req, res) => {
+  const loginQ = req.body.login || req.query.login
+  const receiptsQ = req.body.receipts || req.query.receipts
+  // Receipts.findOne({userLogin: loginQ}, (err, obj) => {
+  //   if (err) {
+  //     console.log(err);
+  //     return res.sendStatus(500);
+  //   }
+  //   let result = false
+  //   const index = obj.receipts.indexOf(receiptsQ)
+  //   if (index !== -1) {
+  //     result = true
+  //   } 
+  //   console.log('result' + result)
+  //   return res.send(result);
+  // })
+
+  docClient.get(params, function (err, data) {
+    if (err) {
+      console.log(err);
+      handleError(err, res);
+    } else {
+      handleSuccess(data.Item, res);
+    }
+  });
+})
 
 //   mongoose.set('useCreateIndex', true);
 
@@ -110,4 +126,14 @@ const port = process.env.PORT || 3000;
 //   });
 app.use('/', dynamoRoute);
 app.listen(port);
-//   console.log('Server running on port 3000');
+function handleError(err, res) {
+  res.json({
+    'message': 'server side error', statusCode: 500, error:
+      err
+  });
+}
+
+function handleSuccess(data, res) {
+  res.json({ message: 'success', statusCode: 200, data: data })
+}
+console.log('Server running on port 3000');
