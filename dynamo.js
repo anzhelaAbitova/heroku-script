c8fa040d48d50e3650b1c02ca4f3ec0c14604d30
anzhelaAbitova <55100446+anzhelaAbitova@users.noreply.github.com>
2022-11-13 13:25:37 +0400

    package json install

--- dynamo.js
+++ dynamo.js
@@ -1,19 +1,19 @@
 
-let express = require('express');
-let router = express.Router();
+// let express = require('express');
+// let router = express.Router();
 
-let docClient = new AWS.DynamoDB.DocumentClient();
-let table = "sports";
+// let docClient = new AWS.DynamoDB.DocumentClient();
+// let table = "sports";
 
-router.get('/fetch', (req, res) => {
+// router.get('/fetch', (req, res) => {
 
-    let spid = '101';
-    let params = {
-        TableName: table,
-        Key: {
-            spid: spid
-        }
-    };
+//     let spid = '101';
+//     let params = {
+//         TableName: table,
+//         Key: {
+//             spid: spid
+//         }
+//     };
 
 
-});
+// });
