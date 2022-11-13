72201103c69e5f69789d1deb48ac3000c020540f
anzhelaAbitova <55100446+anzhelaAbitova@users.noreply.github.com>
2022-11-13 15:12:18 +0400

    create new db

--- index.js
+++ index.js
@@ -1,139 +1,210 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   require('dotenv').config()
+// }
 
-const express = require('express')
-const app = express()
-let AWS = require('aws-sdk');
-let docClient = new AWS.DynamoDB.DocumentClient();
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
-const flash = require('express-flash')
-const cors = require('cors');
-// const Receipts = require('./models/receipts').Receipts;
-let dynamoRoute = require('./dynamo');
-const host = '127.0.0.1'
-const port = process.env.PORT || 3000;
+// const express = require('express')
+// const app = express()
+// let AWS = require('aws-sdk');
+// let docClient = new AWS.DynamoDB.DocumentClient();
+// const bodyParser = require('body-parser');
+// const methodOverride = require('method-override');
+// const flash = require('express-flash')
+// const cors = require('cors');
+// const { v4: uuidv4 } = require('uuid');
+// // const Receipts = require('./models/receipts').Receipts;
+// const tableName = 'frantic-puce-earmuffsCyclicDB';
+// const host = '127.0.0.1'
+// const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(flash())
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(flash())
+// // app.use(cors())
+// app.use(methodOverride('_method'))
 // app.use(cors())
-app.use(methodOverride('_method'))
-app.use(cors())
 
-app.use(async (req, res, next) => {
-  if (req.xhr) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://anastashasuvorova.ru/')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-    )
-    next()
-  } else {
-    res.status(400).end('400 Bad Request')
-  }
-})
+// app.use(async (req, res, next) => {
+//   if (req.xhr) {
+//     res.setHeader('Access-Control-Allow-Origin', 'https://anastashasuvorova.ru/')
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
+//     res.setHeader('Access-Control-Allow-Credentials', true)
+//     res.setHeader(
+//       'Access-Control-Allow-Headers',
+//       'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+//     )
+//     next()
+//   } else {
+//     res.status(400).end('400 Bad Request')
+//   }
+// })
 
-app.post('/receipts', async (req, res) => {
-  try {
-    const loginQ = req.body.login || req.query.login
-    const receiptsQ = req.body.receipts || req.query.receipts
-    console.log(req.query)
-    let search = ''
-    // Receipts.findOne({userLogin: loginQ}, (err, obj) => {
-    //     if(err){
-    //         return console.log(err);
-    //     } 
-    //     if (!obj) {
-    //         const receipt = new Receipts ({
-    //             userLogin: loginQ,
-    //             receipts: [receiptsQ]
-    //           })
-    //          receipt.save(function(err){
-    //             if(err) return console.log(err);
-    //             console.log("Сохранен объект", receipt);
-    //         });
-    //     } else {
-    //         const index = obj.receipts.indexOf(receiptsQ)
+// app.post('/receipts', async (req, res) => {
+//   try {
+//     const loginQ = req.body.login || req.query.login
+//     const receiptsQ = req.body.receipts || req.query.receipts
+//     console.log(req.query)
+//     let search = ''
+//     // Receipts.findOne({userLogin: loginQ}, (err, obj) => {
+//     //     if(err){
+//     //         return console.log(err);
+//     //     } 
+//     //     if (!obj) {
+//     //         const receipt = new Receipts ({
+//     //             userLogin: loginQ,
+//     //             receipts: [receiptsQ]
+//     //           })
+//     //          receipt.save(function(err){
+//     //             if(err) return console.log(err);
+//     //             console.log("Сохранен объект", receipt);
+//     //         });
+//     //     } else {
+//     //         const index = obj.receipts.indexOf(receiptsQ)
 
-    //         if (index === -1) {
-    //           console.log('index ' + index)
-    //           obj.receipts.push(receiptsQ)
-    //         } else {
-    //           obj.receipts.splice(index, 1);
-    //         }
-    //         console.log(obj.receipts)
-    //         return obj.save()
-    //     }
-    // })
+//     //         if (index === -1) {
+//     //           console.log('index ' + index)
+//     //           obj.receipts.push(receiptsQ)
+//     //         } else {
+//     //           obj.receipts.splice(index, 1);
+//     //         }
+//     //         console.log(obj.receipts)
+//     //         return obj.save()
+//     //     }
+//     // })
+//     var params = {
+//       TableName: tableName,
+//       Item: {
+//         "Id": uuidv4(),
+//         "Name": body["name"]
+//       }
+//     };
 
-  } catch (err) {
-    console.log(err);
-    return res.sendStatus(500);
-  }
-})
+//     client.put(params, (err, data) => {
+//       if (err) {
+//         console.error("Unable to add item.");
+//         console.error("Error JSON:", JSON.stringify(err, null, 2));
+//       } else {
+//         console.log("Added item:", JSON.stringify(data, null, 2));
+//       }
+//     });
+//   } catch (err) {
+//     console.log(err);
+//     return res.sendStatus(500);
+//   }
+// })
 
-app.get('/receipts', (req, res) => {
-  const loginQ = req.body.login || req.query.login
-  Receipts.findOne({ userLogin: loginQ }, (err, docs) => {
-    if (err) {
-      console.log(err);
-      return res.sendStatus(500);
-    }
-    console.log(docs.receipts)
-    return res.send(docs.receipts);
-  })
-})
+// app.get('/receipts', (req, res) => {
+//   const loginQ = req.body.login || req.query.login
+//   Receipts.findOne({ userLogin: loginQ }, (err, docs) => {
+//     if (err) {
+//       console.log(err);
+//       return res.sendStatus(500);
+//     }
+//     console.log(docs.receipts)
+//     return res.send(docs.receipts);
+//   })
+// })
 
-app.get('/receipt-for-one', (req, res) => {
-  const loginQ = req.body.login || req.query.login
-  const receiptsQ = req.body.receipts || req.query.receipts
-  // Receipts.findOne({userLogin: loginQ}, (err, obj) => {
-  //   if (err) {
-  //     console.log(err);
-  //     return res.sendStatus(500);
-  //   }
-  //   let result = false
-  //   const index = obj.receipts.indexOf(receiptsQ)
-  //   if (index !== -1) {
-  //     result = true
-  //   } 
-  //   console.log('result' + result)
-  //   return res.send(result);
-  // })
+// app.get('/receipt-for-one', (req, res) => {
+//   const loginQ = req.body.login || req.query.login
+//   const receiptsQ = req.body.receipts || req.query.receipts
+//   // Receipts.findOne({userLogin: loginQ}, (err, obj) => {
+//   //   if (err) {
+//   //     console.log(err);
+//   //     return res.sendStatus(500);
+//   //   }
+//   //   let result = false
+//   //   const index = obj.receipts.indexOf(receiptsQ)
+//   //   if (index !== -1) {
+//   //     result = true
+//   //   } 
+//   //   console.log('result' + result)
+//   //   return res.send(result);
+//   // })
 
-  docClient.get(params, function (err, data) {
-    if (err) {
-      console.log(err);
-      handleError(err, res);
-    } else {
-      handleSuccess(data.Item, res);
-    }
-  });
-})
-
-//   mongoose.set('useCreateIndex', true);
-
-//   const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
-//   mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
-//   const db = mongoose.connection;
-//   db.on('error', console.error.bind(console, 'connection error:'));
-//   db.once('open', function() {
-//     console.log(`we're connected!`);
+//   docClient.get(params, function (err, data) {
+//     if (err) {
+//       console.log(err);
+//       handleError(err, res);
+//     } else {
+//       handleSuccess(data.Item, res);
+//     }
 //   });
+// })
 
-app.listen(port);
-function handleError(err, res) {
-  res.json({
-    'message': 'server side error', statusCode: 500, error:
-      err
-  });
-}
+// //   mongoose.set('useCreateIndex', true);
 
-function handleSuccess(data, res) {
-  res.json({ message: 'success', statusCode: 200, data: data })
-}
-console.log('Server running on port 3000');
+// //   const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+// //   mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+// //   const db = mongoose.connection;
+// //   db.on('error', console.error.bind(console, 'connection error:'));
+// //   db.once('open', function() {
+// //     console.log(`we're connected!`);
+// //   });
+
+// app.listen(port);
+// function handleError(err, res) {
+//   res.json({
+//     'message': 'server side error', statusCode: 500, error:
+//       err
+//   });
+// }
+
+// function handleSuccess(data, res) {
+//   res.json({ message: 'success', statusCode: 200, data: data })
+// }
+// console.log('Server running on port 3000');
+
+
+// Create service client module using ES6 syntax.
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+// Set the AWS Region.
+const REGION = process.env.REGION; 
+// Create an Amazon DynamoDB service client object.
+const ddbClient = new DynamoDBClient({ region: REGION });
+
+// Import required AWS SDK clients and commands for Node.js
+import { CreateTableCommand } from "@aws-sdk/client-dynamodb";
+
+
+// Set the parameters
+export const params = {
+  AttributeDefinitions: [
+    {
+      AttributeName: "userLogin", //ATTRIBUTE_NAME_1
+      AttributeType: "S", //ATTRIBUTE_TYPE
+    },
+    {
+      AttributeName: "Episode", //ATTRIBUTE_NAME_2
+      AttributeType: "SS", //ATTRIBUTE_TYPE
+    },
+  ],
+  KeySchema: [
+    {
+      AttributeName: "userLogin", //ATTRIBUTE_NAME_1
+      KeyType: "HASH",
+    },
+    {
+      AttributeName: "receipts", //ATTRIBUTE_NAME_2
+      KeyType: "RANGE",
+    },
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 1,
+    WriteCapacityUnits: 1,
+  },
+  TableName: "frantic-puce-earmuffsCyclicDB", //TABLE_NAME
+  StreamSpecification: {
+    StreamEnabled: false,
+  },
+};
+
+export const run = async () => {
+  try {
+    const data = await ddbClient.send(new CreateTableCommand(params));
+    console.log("Table Created", data);
+    return data;
+  } catch (err) {
+    console.log("Error", err);
+  }
+};
+run();
